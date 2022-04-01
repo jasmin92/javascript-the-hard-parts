9c45a03ae622fe8877bd754eb3a81256f4797cc8
Jasmin Durakovic <jasmindurakovic13@gmail.com>
2022-04-01 15:56:41 +0200

    intersection test

--- callbacks/__tests__/index.test.js
+++ callbacks/__tests__/index.test.js
@@ -1,5 +1,14 @@
 import { expect, test } from "vitest"
-import { addS, addTwo, multipleByTwo, map, forEach, reduce } from ".."
+import {
+  addS,
+  addTwo,
+  multipleByTwo,
+  map,
+  forEach,
+  reduce,
+  intersectionOfTwoArrays,
+  intersection,
+} from ".."
 
 test("addTwo", () => {
   const result = addTwo(1)
@@ -28,7 +37,18 @@ test("forEach", () => {
 })
 
 test("reduce", () => {
-  let result = null
-  result = reduce([1, 2, 3], (a, b) => a + b, 0)
+  const result = reduce([1, 2, 3], (a, b) => a + b, 0)
   expect(result).toBe(6)
+})
+
+test("intersection", () => {
+  const array1 = [1, 2]
+  const array2 = [1, 2, 3]
+  const array3 = [1, 4, 5, 7]
+  const array4 = [6, 3, 3]
+
+  expect(intersectionOfTwoArrays(array1, array3)).toEqual([1])
+  expect(intersection(array1, array2)).toEqual([1, 2])
+  expect(intersection(array1, array2, array3)).toEqual([1])
+  expect(intersection(array1, array4)).toEqual([])
 })
