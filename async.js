500c7591525db1eac131e6afb6e1973c185d4b88
Jasmin Durakovic <jasmindurakovic13@gmail.com>
2022-04-17 22:19:13 +0200

    delayCounter

--- async.js
+++ async.js
@@ -13,3 +13,27 @@ export function limitedRepeat() {
     }
   }, 1000)
 }
+
+export function everyXsecsForYsecs(func, interval, duration) {
+  let elapsed = 0
+  const intervalId = setInterval(() => {
+    if (elapsed >= duration) {
+      clearInterval(intervalId)
+      return
+    }
+
+    func()
+    elapsed += interval
+  }, interval)
+}
+
+export function delayCounter(target, wait) {
+  let index = 1
+  const interval = setInterval(() => {
+    if (index > target) {
+      clearInterval(interval)
+      return
+    }
+    console.log(index++)
+  }, wait)
+}
